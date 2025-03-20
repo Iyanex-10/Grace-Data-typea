0cf864485ba602d5fdebb89b8d7192e8b8b4c4a6
Iyanex-10 <graceshodeinde2010@gmail.com>
2025-03-20 12:13:18 +0100

    Add files via upload

---
+++ Grace Data Types Assignment.py
@@ -0,0 +1,55 @@
+# task 1
+print(type(1))
+print(type('sister'))
+print(type(12.5))
+print(type(2+3j))
+# task 2
+a= 53
+b= 10
+sum=a+b
+print(sum)
+c= 21
+d= 25
+sub=c-d
+print(sub)
+g= 8
+h= 2.5
+mul=g*h
+print(mul)
+# task 3
+num1 = 56
+float1 = 25.56
+num_to_float = float(num1)
+float_to_num = int(float1)
+print(num_to_float)
+print(float_to_num)
+# task 4
+word = ("Hi there")
+print(word.upper())
+text = ("MY NAME")
+print(text.lower())
+new_string = word + text
+print(new_string)
+# task 5
+y = "Hello, " + "name" + "!"
+print(y)
+name = "grace"
+age = 29
+print("hello,"+"name"+ "! You are" + str(age)+ "years old")
+List1 = [2, 3, 4, 5]
+List2 = [7, 9, 11, 13]
+a = List1[0]
+print (a)
+b = List1[0:2]
+print(b)
+fruits = ["orange", "apple", "banana"]
+fruits.append("cherry")
+print(fruits)
+fruits.remove("orange")
+print(fruits)
+fruits.reverse("apple",-1)
+# task 6
+My_tuple = (1, 2, 3, 4, 5)
+print(My_tuple)
+My_tuple(0)= 2              it shows that it is immutable
+print(My_tuple)
